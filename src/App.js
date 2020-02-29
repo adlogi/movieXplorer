1cbede16c0737c370fa6109822fa1e12026ce463
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-29 15:14:54 +0300

    adding search functionality

--- src/App.js
+++ src/App.js
@@ -13,10 +13,29 @@ import MovieList from './MovieList';
 // export default App;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchKeywords: '',
+    }
+  }
+  clickHandle = () => {
+    this.setState({
+      searchKeywords: document.querySelector('#search-box').value,
+    })
+    
+  }
+
   render() {
+    console.log(`STATE: ${this.state.searchKeywords}`)
     return (
       <div className="App">
-        <MovieList />
+        <div className="App-header">
+          <input id="search-box" type="text" placeholder="Enter a keyword"></input>
+          <button onClick={this.clickHandle}>Find</button>
+          
+        </div>
+        <MovieList searchKeywords={this.state.searchKeywords} />
       </div>
     );
   }
--- src/MovieList.js
+++ src/MovieList.js
@@ -6,8 +6,12 @@ export default class MovieList extends React.Component {
   static API_KEY = '542003918769df50083a13c415bbc602';
   
 
-  static  _constructUrl(path) {
+  static  _constructDefaultUrl(path) {
     return `${MovieList.TMDB_BASE_URL}/${path}?api_key=${MovieList.API_KEY}`;
+  }
+
+  static  _constructSearchUrl(keywords) {
+    return `${MovieList.TMDB_BASE_URL}/search/movie?api_key=${MovieList.API_KEY}&language=en-US&query=${keywords}&page=1&include_adult=false`;
   }
 
   constructor(props) {
@@ -19,54 +23,72 @@ export default class MovieList extends React.Component {
     };
   }
 
-  componentDidMount() {
-    // Fetch data from TMDB website directly:
-    // const url = MovieList._constructUrl(`movie/now_playing`);
-    // Fetch data from a local file in /public:
-    const url = './movies.json';
-    console.log(url);
+  retrieveData() {
+    let url;
+    if (this.props.searchKeywords === '') {
+      // url = './movies.json';
+      url = MovieList._constructDefaultUrl(`movie/now_playing`);
+    } else {
+      url = MovieList._constructSearchUrl(this.props.searchKeywords)
+    }
+    
     fetch(url)
-      .then(res => res.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoaded: true,
-            movies: data.results
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    .then(res => res.json())
+    .then(
+      data => {
+        console.log(`Update: ${data.results}`)
+        this.setState({
+          isLoaded: true,
+          movies: data.results
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.props.searchKeywords !== nextProps.searchKeywords) return true;
+  //   return false;
+  // }
+
+  componentDidMount() {
+    this.retrieveData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.searchKeywords !== prevProps.searchKeywords) {
+      this.retrieveData();
+    }
+  }
+
+  generateMovieList(movies) {
+    return (
+      <div>
+      {
+        movies.map(movie =>
+            <div key={movie.id}>
+              <h2>{movie.title} <small>({movie.release_date.slice(0, 4)})</small></h2>
+              <img src={MovieList.BACKDROP_BASE_URL + movie.poster_path} alt={movie.title} width="200"></img>
+            </div>
+          )
+      }
+      </div>
+    );
   }
 
   render() {
     const { error, isLoaded, movies } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading Movies...</div>;
-    } else {
-      return (
-        <div>
-        {
-          movies.map(movie => {
-            return (
-              <div key={movie.id}>
-                <h2>{movie.title} <small>({movie.release_date.slice(0, 4)})</small></h2>
-                <img src={MovieList.BACKDROP_BASE_URL + movie.poster_path} alt={movie.title} width="200"></img>
-              </div>
-            )
-          })
-        }
-        </div>
-      );
-    }
+
+    if (error) return <div>Error: {error.message}</div>;
+    if (!isLoaded) return <div>Loading Movies...</div>;
+    return this.generateMovieList(movies);
   }
 }
