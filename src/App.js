84cd79e1d47ab735e65f6ffb58d03dc5d9813e8e
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-29 20:59:14 +0300

    show movies using BS cardDeck

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import MovieList from './MovieList';
 import SearchBox from './SearchBox';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from './logo.png'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,6 +23,17 @@ export default class App extends React.Component {
     console.log(`App(MOVIES): ${this.state.movies}`)
     return (
       <div className="App">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="100"
+              // height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+        </Navbar>
         {/* <div className="App-header"> */}
         <div>
           <SearchBox setAppState={this.setAppState} />
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
 
 export default class MovieList extends React.Component {
   static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780';
 
-  generateMovieList(movies) {
-    return (
-      <div>
-      {
-        movies.map(movie =>
-            <div key={movie.id}>
-              <h2>{movie.title} <small>({movie.release_date.slice(0, 4)})</small></h2>
-              <img src={MovieList.BACKDROP_BASE_URL + movie.poster_path} alt={movie.title} width="200"></img>
-            </div>
-          )
-      }
-      </div>
-    );
+  
+  generateMovieCardDeck(movies) {
+    let movieCards = [];
+
+    for (let i = 0; i < movies.length; i++) {
+      movieCards.push(
+        <Card key={movies[i].id} className="mt-3">
+          <Card.Img variant="top" src={MovieList.BACKDROP_BASE_URL + movies[i].poster_path} />
+          <Card.Body>
+            <Card.Title>{movies[i].title}</Card.Title>
+            <Card.Text>Rating: {movies[i].vote_average} / 10</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">({movies[i].release_date.slice(0, 4)})</small>
+          </Card.Footer>
+        </Card>
+      );
+      // wrap every 2 on sm
+      if ((i + 1) % 2 === 0) movieCards.push(<div class="w-100 d-none d-sm-block d-md-none"></div>);
+      // wrap every 3 on md
+      if ((i + 1) % 3 === 0) movieCards.push(<div class="w-100 d-none d-md-block d-lg-none"></div>);
+      // wrap every 4 on lg
+      if ((i + 1) % 4 === 0) movieCards.push(<div class="w-100 d-none d-lg-block d-xl-none"></div>);
+      // wrap every 5 on xl
+      if ((i + 1) % 5 === 0) movieCards.push(<div class="w-100 d-none d-xl-block"></div>);
+      // Credit: https://www.codeply.com/go/nIB6oSbv6q
+    }
+    return <CardDeck>{ movieCards }</CardDeck>;
   }
 
   render() {
-    return this.generateMovieList(this.props.movies);
+    return this.generateMovieCardDeck(this.props.movies);
   }
 }
