31e7d35dd92d42fc8de629d63d1fe23677ca19f3
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-02 23:57:02 +0300

    add a button at the bottom to load more results

--- src/App.js
+++ src/App.js
@@ -14,33 +14,37 @@ export default class App extends React.Component {
     this.state = {
       movies: [],
       searchKeywords: '',
+      lastPageLoaded: 0,
+      totalPages: 0,
     }
   }
 
-  constructDefaultUrl(path) {
-    return `${App.TMDB_BASE_URL}/${path}?api_key=${App.API_KEY}`;
+  constructDefaultUrl(param, page = 1) {
+    return `${App.TMDB_BASE_URL}/movie/${param}?api_key=${App.API_KEY}&language=en-US&page=${page}`;
   }
 
-  constructSearchUrl(keywords) {
-    return `${App.TMDB_BASE_URL}/search/movie?api_key=${App.API_KEY}&language=en-US&query=${keywords}&page=1&include_adult=false`;
+  constructSearchUrl(keywords, page = 1) {
+    return `${App.TMDB_BASE_URL}/search/movie?api_key=${App.API_KEY}&language=en-US&query=${keywords}&page=${page}&include_adult=false`;
   }
 
-  retrieveData = (searchKeywords) => {
+  retrieveData = (searchKeywords, page = 1) => {
     let url;
     if (searchKeywords === '') {
-      url = this.constructDefaultUrl(`movie/popular`);
+      url = this.constructDefaultUrl(`popular`, page);
     } else {
-      url = this.constructSearchUrl(searchKeywords);
+      url = this.constructSearchUrl(searchKeywords, page);
     }
     
     fetch(url)
     .then(res => res.json())
     .then(
       data => {
-        this.setState({
-          movies: data.results,
+        this.setState(prevState => ({
+          movies: (page === 1 ? [] : prevState.movies).concat(data.results),
           searchKeywords: searchKeywords,
-        })
+          totalPages: data.total_pages,
+          lastPageLoaded: page,
+        }))
       }
       // ,
       // TODO: Note: it's important to handle errors here
@@ -71,17 +75,20 @@ export default class App extends React.Component {
           {this.state.searchKeywords === '' ? 'The Most Popular Today!' : `Results for "${this.state.searchKeywords}"`}
         </h1>
         <MovieList movies={this.state.movies} />
-        <footer className="p-1"></footer>
+        {(this.state.lastPageLoaded < this.state.totalPages)?
+        (<button onClick={this.onLoadMore} >Load more...</button>):''}
+        <footer className="p-4"></footer>
       </div>
     );
   }
 
-  componentDidMount() {
-    this.retrieveData('');
-    setTimeout(this.showState, 2000);
+  onLoadMore = () => {
+    if (this.state.lastPageLoaded < this.state.totalPages) {
+      this.retrieveData(this.state.searchKeywords, this.state.lastPageLoaded + 1);
+    }
   }
 
-  showState = () => {
-    console.log(`Movies: ${this.state.movies}`);
+  componentDidMount() {
+    this.retrieveData('');
   }
 }
