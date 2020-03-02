3302c08d259cd04b3293883864f137084567c35a
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-03 00:38:20 +0300

    fixing keys of the movie list elements

--- src/MovieList.js
+++ src/MovieList.js
@@ -13,7 +13,8 @@ export default class MovieList extends React.Component {
       let posterSrc = movies[i].poster_path === null ? genericPoster : (MovieList.BACKDROP_BASE_URL + movies[i].poster_path);
       let releaseDate = movies[i].release_date === undefined ? 'UNKNOWN' : movies[i].release_date.slice(0, 4);
       movieCards.push(
-        <Card key={movies[i].id} className="mt-3">
+        // <Card key={movies[i].id} className="mt-3">
+        <Card key={i} className="mt-3">
           <Card.Img variant="top" src={posterSrc} />
           <Card.Body>
             <Card.Title>{movies[i].title}</Card.Title>
@@ -30,13 +31,13 @@ export default class MovieList extends React.Component {
       // Controling the number of cards per row (responsive)
       // Credit: https://www.codeply.com/go/nIB6oSbv6q
       // wrap every 2 on sm
-      if ((i + 1) % 2 === 0) movieCards.push(<div className="w-100 d-none d-sm-block d-md-none"></div>);
+      if ((i + 1) % 2 === 0) movieCards.push(<div key={`${i}.2`} className="w-100 d-none d-sm-block d-md-none"></div>);
       // wrap every 3 on md
-      if ((i + 1) % 3 === 0) movieCards.push(<div className="w-100 d-none d-md-block d-lg-none"></div>);
+      if ((i + 1) % 3 === 0) movieCards.push(<div key={`${i}.3`} className="w-100 d-none d-md-block d-lg-none"></div>);
       // wrap every 4 on lg
-      if ((i + 1) % 4 === 0) movieCards.push(<div className="w-100 d-none d-lg-block d-xl-none"></div>);
+      if ((i + 1) % 4 === 0) movieCards.push(<div key={`${i}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
       // wrap every 5 on xl
-      if ((i + 1) % 5 === 0) movieCards.push(<div className="w-100 d-none d-xl-block"></div>);
+      if ((i + 1) % 5 === 0) movieCards.push(<div key={`${i}.5`} className="w-100 d-none d-xl-block"></div>);
     }
     return <CardDeck className="m-4 pb-5">{ movieCards }</CardDeck>;
   }
