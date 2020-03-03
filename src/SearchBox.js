127a449cfdf90d2351f622a80bef0fbe72122c4d
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-03 12:16:17 +0300

    rewriting SearchBox as a functional componant

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 
-export default class SearchBox extends React.Component {
-
-  handleSearch = () => {
+const SearchBox = (props) => {
+  const handleSearch = () => {
     const searchBox = document.querySelector('#search-box');
     searchBox.blur();
-    this.props.handleSearch(searchBox.value);
+    props.handleSearch(searchBox.value);
   }
 
-  handleSearchFocus = (event) => {
+  const handleSearchFocus = (event) => {
     event.target.value = '';
   }
 
-  handleEnter = (event) => {
+  const handleEnter = (event) => {
     if (event.keyCode === 13) {
       event.preventDefault();
       document.querySelector('#button-addon2').click();
     }
   }
 
-  render() {
-    return (
-      <div className="bg-light rounded rounded-pill shadow-sm m-5 p-2 w-75">
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <button onClick={this.handleSearch} id="button-addon2" type="submit" className="btn btn-link text-dark"><i className="fa fa-search"></i></button>
-          </div>
-          <input id="search-box" type="search" placeholder="What're you searching for?" aria-describedby="button-addon2" className="form-control border-0 bg-light" onKeyUp={this.handleEnter} onFocus={this.handleSearchFocus} />
+  return (
+    <div className="bg-light rounded rounded-pill shadow-sm m-5 p-2 w-75">
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <button onClick={handleSearch} id="button-addon2" type="submit" className="btn btn-link text-dark"><i className="fa fa-search"></i></button>
         </div>
+        <input id="search-box" type="search" placeholder="What're you searching for?" aria-describedby="button-addon2" className="form-control border-0 bg-light" onKeyUp={handleEnter} onFocus={handleSearchFocus} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default SearchBox;
