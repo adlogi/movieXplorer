0fe2b82d4477248b695bfc42c2d06e59bdfbcb2e
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-09 03:02:42 +0300

    fixing loading bug

--- src/MovieList.js
+++ src/MovieList.js
@@ -5,14 +5,10 @@ import Placeholder from './Placeholder'
 
 export default function MovieList(props) {
   const movieCards = [];
-  const cardsCount = props.isLoading ? 10 : props.movies.length;
+  let i = 0;
 
-  for (let i = 0; i < cardsCount; i++) {
-    if (props.isLoading) {
-      movieCards.push(<Placeholder key={i} />);
-    } else {
-      movieCards.push(<MovieCard key={i} movie={props.movies[i]} detailsHandler={props.detailsHandler} />);
-    }
+  for (; i < props.movies.length; i++) {
+    movieCards.push(<MovieCard key={i} movie={props.movies[i]} detailsHandler={props.detailsHandler} />);
     // Controling the number of cards per row (responsive)
     // Credit: https://www.codeply.com/go/nIB6oSbv6q
     // wrap every 2 on sm
@@ -23,6 +19,15 @@ export default function MovieList(props) {
     if ((i + 1) % 4 === 0) movieCards.push(<div key={`${i}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
     // wrap every 5 on xl
     if ((i + 1) % 5 === 0) movieCards.push(<div key={`${i}.5`} className="w-100 d-none d-xl-block"></div>);
+  }
+  if (props.isLoading) {
+    for (; i < props.movies.length + 10; i++) {
+      movieCards.push(<Placeholder key={i} />);
+      if ((i + 1) % 2 === 0) movieCards.push(<div key={`${i}.2`} className="w-100 d-none d-sm-block d-md-none"></div>);
+      if ((i + 1) % 3 === 0) movieCards.push(<div key={`${i}.3`} className="w-100 d-none d-md-block d-lg-none"></div>);
+      if ((i + 1) % 4 === 0) movieCards.push(<div key={`${i}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
+      if ((i + 1) % 5 === 0) movieCards.push(<div key={`${i}.5`} className="w-100 d-none d-xl-block"></div>);
+    }
   }
   
   return <CardDeck className="m-4 pb-5">{ movieCards }</CardDeck>;
