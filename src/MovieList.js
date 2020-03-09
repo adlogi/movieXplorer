e04f9db2e980eb772a4ecc59dca5eb25b00eac33
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-09 05:29:56 +0300

    code cleanup

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,28 +7,29 @@ export default function MovieList(props) {
   const movieCards = [];
   let i = 0;
 
-  for (; i < props.movies.length; i++) {
-    movieCards.push(<MovieCard key={i} movie={props.movies[i]} detailsHandler={props.detailsHandler} />);
+  function insertRowBreak(index) {
     // Controling the number of cards per row (responsive)
     // Credit: https://www.codeply.com/go/nIB6oSbv6q
     // wrap every 2 on sm
-    if ((i + 1) % 2 === 0) movieCards.push(<div key={`${i}.2`} className="w-100 d-none d-sm-block d-md-none"></div>);
+    if ((index + 1) % 2 === 0) movieCards.push(<div key={`${index}.2`} className="w-100 d-none d-sm-block d-md-none"></div>);
     // wrap every 3 on md
-    if ((i + 1) % 3 === 0) movieCards.push(<div key={`${i}.3`} className="w-100 d-none d-md-block d-lg-none"></div>);
+    if ((index + 1) % 3 === 0) movieCards.push(<div key={`${index}.3`} className="w-100 d-none d-md-block d-lg-none"></div>);
     // wrap every 4 on lg
-    if ((i + 1) % 4 === 0) movieCards.push(<div key={`${i}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
+    if ((index + 1) % 4 === 0) movieCards.push(<div key={`${index}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
     // wrap every 5 on xl
-    if ((i + 1) % 5 === 0) movieCards.push(<div key={`${i}.5`} className="w-100 d-none d-xl-block"></div>);
+    if ((index + 1) % 5 === 0) movieCards.push(<div key={`${index}.5`} className="w-100 d-none d-xl-block"></div>);
+  }
+
+  for (; i < props.movies.length; i++) {
+    movieCards.push(<MovieCard key={i} movie={props.movies[i]} detailsHandler={props.detailsHandler} />);
+    insertRowBreak(i);
   }
   if (props.isLoading) {
     for (; i < props.movies.length + 10; i++) {
       movieCards.push(<Placeholder key={i} />);
-      if ((i + 1) % 2 === 0) movieCards.push(<div key={`${i}.2`} className="w-100 d-none d-sm-block d-md-none"></div>);
-      if ((i + 1) % 3 === 0) movieCards.push(<div key={`${i}.3`} className="w-100 d-none d-md-block d-lg-none"></div>);
-      if ((i + 1) % 4 === 0) movieCards.push(<div key={`${i}.4`} className="w-100 d-none d-lg-block d-xl-none"></div>);
-      if ((i + 1) % 5 === 0) movieCards.push(<div key={`${i}.5`} className="w-100 d-none d-xl-block"></div>);
+      insertRowBreak(i);
     }
   }
-  
+
   return <CardDeck className="m-4 pb-5">{ movieCards }</CardDeck>;
 }
--- src/Placeholder.js
+++ src/Placeholder.js
@@ -1,38 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import ContentLoader from "react-content-loader";
 import Card from 'react-bootstrap/Card';
 import genericPoster from './film-poster-placeholder.png';
 
-export default class Placeholder extends Component {
-  render() {
-    return (
-      <Card className="my-3">
-        <img alt="loading" className="feed__loading-item" src={genericPoster} width="100%" />
-        <Card.Body>
-          <ContentLoader
-            width={100+'%'}
-            height={60}
-            speed={2}
-            primary-color="#f3f3f3"
-            secondary-color="#7c7c7c"
-            style={{ marginBottom: "4px" }}
-          >
-            <rect x="5%" y="10" rx="10" ry="10" width="90%" height="40" />
-          </ContentLoader>
-        </Card.Body>
-        <Card.Footer>
-          <ContentLoader
-            width={100+'%'}
-            height={20}
-            speed={2}
-            primary-color="#f3f3f3"
-            secondary-color="#7c7c7c"
-            style={{ marginBottom: "4px" }}
-          >
-            <rect x="5%" y="5" rx="10" ry="10" width="90%" height="10" />
-          </ContentLoader>
-        </Card.Footer>
-      </Card>    
-    );
-  }
+export default function Placeholder() {
+  return (
+    <Card className="my-3">
+      <img alt="loading" className="feed__loading-item" src={genericPoster} width="100%" />
+      <Card.Body>
+        <ContentLoader
+          width={100+'%'}
+          height={60}
+          speed={2}
+          primary-color="#f3f3f3"
+          secondary-color="#7c7c7c"
+          style={{ marginBottom: "4px" }}
+        >
+          <rect x="5%" y="10" rx="10" ry="10" width="90%" height="40" />
+        </ContentLoader>
+      </Card.Body>
+      <Card.Footer>
+        <ContentLoader
+          width={100+'%'}
+          height={20}
+          speed={2}
+          primary-color="#f3f3f3"
+          secondary-color="#7c7c7c"
+          style={{ marginBottom: "4px" }}
+        >
+          <rect x="5%" y="5" rx="10" ry="10" width="90%" height="10" />
+        </ContentLoader>
+      </Card.Footer>
+    </Card>    
+  );
 }
