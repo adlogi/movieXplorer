121b6dc08c588f328a34c34d30d4b888ceb26b9d
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-10 18:25:33 +0300

    Creating a component to show a poster loading

---
+++ src/components/PosterLoading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import genericPoster from '../media/film-poster-placeholder.png';
+import '../style/PosterLoading.css';
+
+export default function PosterLoading(props) {
+  return (
+    <div className="loader-container">
+      <img alt="loading" className="feed__loading-item" src={genericPoster} width="100%" />
+      <div className="loader" onClick={() => props.detailsHandler(props.movieId)}>
+        <ul>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+        </ul>
+      </div>
+    </div>  
+  );
+}
