ad1fae230d0934ee5e227d6f09c94d1e5954b942
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-22 12:54:45 +0300

    Updates

---
+++ precache-manifest.8811f1fbec347042ebd9d51a78e2bd65.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161ad937e98a5df79477ac94c8c18c5a",
+    "url": "/moviexplorer/index.html"
+  },
+  {
+    "revision": "d788361d134fc44c8ec9",
+    "url": "/moviexplorer/static/css/2.f61e5bef.chunk.css"
+  },
+  {
+    "revision": "c9f5fb79069457ac178f",
+    "url": "/moviexplorer/static/css/main.6d1eb5ec.chunk.css"
+  },
+  {
+    "revision": "d788361d134fc44c8ec9",
+    "url": "/moviexplorer/static/js/2.fc52b772.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/moviexplorer/static/js/2.fc52b772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f5fb79069457ac178f",
+    "url": "/moviexplorer/static/js/main.34e5c8d8.chunk.js"
+  },
+  {
+    "revision": "7bde2990258e8ce4dfba",
+    "url": "/moviexplorer/static/js/runtime-main.a46c3810.js"
+  },
+  {
+    "revision": "e611409a7d9d0f142fcc6f95e236c92c",
+    "url": "/moviexplorer/static/media/film-poster-placeholder.e611409a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/moviexplorer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/moviexplorer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/moviexplorer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/moviexplorer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/moviexplorer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
